909ac2fb5afaa2dba3deb811f10505315b465546
Dwayne Reed <dreedjr1992@gmail.com>
2022-08-27 20:25:00 -0500

    New commit

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,11 +4,11 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+       System.out.println(sayHello());
     }
 
     public static String sayHello() {
-        return "Hello World!";
+        return "Hello, World!";
     }
 
 }
